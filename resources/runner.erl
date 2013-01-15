53f810d73fde67b61196c5f236119f60e2d28bb6
Patrick Gombert <patrickgombert@gmail.com>
2013-01-14 22:21:19 -0600

    Refactor runner to DRY up commonalities between run and test

--- resources/runner.erl
+++ resources/runner.erl
@@ -1,92 +1,10 @@
 -module(runner).
--export([run/0, test/0, lookup/0]).
-
-run() ->
-  {ok, Config} = file:consult("resources/answers.config"),
-  Output = run_all(Config),
-  case Output of
-    ok ->
-      io:format("You have completed the Erlang Koans. Namaste.\n");
-    {error, {ModuleName, Function, {Expected, Value}}} ->
-      io:format("The following function failed its test:\n"),
-      erlang:display({ModuleName, Function}),
-      io:format("For the following reason:\n"),
-      erlang:display(Expected),
-      erlang:display(Value)
-  end,
-  halt().
-
-run_all(Config) ->
-  if
-    Config == [] -> ok;
-    true ->
-      [Module | Tail] = Config,
-      case run_module(Module) of
-        ok -> run_all(Tail);
-        {error, Reason} -> {error, Reason}
-      end
-  end.
-
-run_module(Config) ->
-  {ModuleName, FunctionToAnswer} = Config,
-  TestModuleName = atom_append(ModuleName, '_test'),
-  ResultList = lists:map(fun({Function, _}) ->
-                   TestFunction = atom_append(Function, '_test'),
-                   try apply(TestModuleName, TestFunction, [])
-                   catch
-                     _:{BadMatch, Reason} ->
-                       case BadMatch of
-                         badmatch -> {error, Function, {badmatch, "A pattern failed to match correctly"}};
-                         _ ->
-                           Expected = lists:keyfind(expected, 1, Reason),
-                           Value = lists:keyfind(value, 1, Reason),
-                           {error, Function, {Expected, Value}}
-                       end
-                   end
-                 end,
-                 FunctionToAnswer),
-  Result = lists:keyfind(error, 1, ResultList),
-  if
-    Result == false -> ok;
-    true ->
-      {_, Function, Reason} = Result,
-      {error, {ModuleName, Function, Reason}}
-  end.
-
-test() ->
-  {ok, Config} = file:consult("resources/answers.config"),
-  test_all(Config),
-  io:format("All tests pass.\n"),
-  halt().
-
-test_all(Config) ->
-  if
-    Config == [] -> ok;
-    true ->
-      [Module | Tail] = Config,
-      case test_module(Module) of
-        ok -> test_all(Tail)
-      end
-  end.
-
-test_module(Config) ->
-  {ModuleName, FunctionToAnswer} = Config,
-  TestModuleName = atom_append(ModuleName, '_test'),
-  lists:foreach(fun({Function, _}) ->
-     TestFunction = atom_append(Function, '_test'),
-     try apply(TestModuleName, TestFunction, [])
-     catch
-      _:Reason ->
-        erlang:display(Reason),
-        halt()
-     end
-   end,
-   FunctionToAnswer).
+-export([lookup/0, run/0, test/0]).
 
 lookup() ->
-  {ok, Config} = file:consult("resources/answers.config"),
-  try throw(a)
-  catch throw:a ->
+  Config = read_config(),
+  try throw(purposeful_error)
+  catch throw:purposeful_error ->
     StackTrace = erlang:get_stacktrace(),
     Caller = lists:nth(2, StackTrace),
     {Module, Function, _, _} = Caller,
@@ -96,6 +14,80 @@ lookup() ->
     Answer
   end.
 
+run() ->
+  Reporter = fun({Module, {Function, _}}) ->
+               TestFunction = atom_append(Function, '_test'),
+               TestModule = atom_append(Module, '_test'),
+               try apply(TestModule, TestFunction, []) of
+                 ok -> {ok, Module, Function, {}}
+               catch
+                 _:{BadMatch, Reason} ->
+                   case BadMatch of
+                     badmatch ->
+                      {error, Module, Function, {badmatch, "A pattern failed to match correctly"}};
+                     _ ->
+                       Expected = lists:keyfind(expected, 1, Reason),
+                       Value = lists:keyfind(value, 1, Reason),
+                       {error, Module, Function, {Expected, Value}}
+                   end;
+                 _:Reason -> {error, Module, Function, {failure, Reason}}
+               end
+             end,
+  Report = excercise_all(read_config(), Reporter),
+  case lists:keyfind(error, 1, Report) of
+    false -> io:format("You have completed the Erlang Koans. Namaste.\n");
+    {error, Module, Function, {Expected, Actual}} ->
+      io:format("The following function failed its test:\n"),
+      erlang:display({Module, Function}),
+      io:format("For the following reason:\n"),
+      erlang:display(Expected),
+      erlang:display(Actual)
+  end,
+  halt().
+
+test() ->
+  Reporter = fun({Module, {Function, _}}) ->
+               TestFunction = atom_append(Function, '_test'),
+               TestModule = atom_append(Module, '_test'),
+               try apply(TestModule, TestFunction, []) of
+                 ok -> {ok, Module, Function, {}}
+               catch
+                 _:Reason ->
+                   {error, Module, Function, Reason}
+               end
+             end,
+  Report = excercise_all(read_config(), Reporter),
+  case lists:keyfind(error, 1, Report) of
+    false -> io:format("All tests pass!\n");
+    _ ->
+      lists:foreach(fun({Status, Module, Error, Reason}) ->
+          if
+            Status == error ->
+              erlang:display(Reason);
+            true -> ok
+          end
+        end, Report)
+  end,
+  halt().
+
+excercise_all(Answers, Reporter) ->
+  excercise_all(Answers, Reporter, []).
+excercise_all(Answers, Reporter, Report) ->
+  if
+    Answers == [] -> Report;
+    true ->
+      [ModuleAnswers | Tail] = Answers,
+      excercise_all(Tail, Reporter, (Report ++ exercise_module(ModuleAnswers, Reporter)))
+  end.
+
+exercise_module(ModuleAnswers, Reporter) ->
+  {ModuleName, FunctionAnswerKey} = ModuleAnswers,
+  lists:map(Reporter, lists:map(fun(FunctionAnswer) -> {ModuleName, FunctionAnswer} end, FunctionAnswerKey)).
+
 atom_append(Atom1, Atom2) ->
   list_to_atom(atom_to_list(Atom1) ++ atom_to_list(Atom2)).
+
+read_config() ->
+  {ok, Config} = file:consult("resources/answers.config"),
+  Config.
 
