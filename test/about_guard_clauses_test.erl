13c8c6809848da6a46a22ac0335adf114b45a798
Patrick Gombert <patrickgombert@gmail.com>
2013-01-31 22:27:50 -0600

    Begin building about guard clauses topic

---
+++ test/about_guard_clauses_test.erl
@@ -0,0 +1,12 @@
+-module(about_guard_clauses_test).
+-include_lib("eunit/include/eunit.hrl").
+
+truthfulness_is_necessary_for_guards_test() ->
+  ?assertEqual(get_here, about_guard_clauses:truthfulness_is_necessary_for_guards()).
+
+there_exists_a_shorthand_for_and_test() ->
+  ?assertEqual(get_here, about_guard_clauses:there_exists_a_shorthand_for_and()).
+
+there_also_exists_a_shorthand_for_or_test() ->
+  ?assertEqual(get_here, about_guard_clauses:there_also_exists_a_shorthand_for_or()).
+
