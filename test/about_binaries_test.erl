e795454d9482330a05fd44ec5576ba2600166220
Patrick Gombert <patrickgombert@gmail.com>
2013-02-08 12:06:49 -0600

    Fix about_binaries_test incorrect method name

--- test/about_binaries_test.erl
+++ test/about_binaries_test.erl
@@ -7,7 +7,7 @@ binaries_of_strings_test() ->
 and_also_strings_of_binaries_test() ->
   ?assert(about_binaries:and_also_strings_of_binaries()).
 
-returning_the_first_byte_test() ->
+the_first_byte_is_a_byte___right_test() ->
   ?assert(about_binaries:the_first_byte_is_a_byte___right()).
 
 then_what_is_the_last_test() ->
